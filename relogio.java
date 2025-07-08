9004dc7f58049b5bf4d99db4c491d2a8282349ee
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-08 19:47:06 -0300

    Update relogio.java

--- relogio.java
+++ relogio.java
@@ -1,16 +1,28 @@
-public class relogio {
+import java.util.Scanner;
+
+public class jonathan1 {
     public static void main(String[] args) {
 
-        for(int h=0; h<24; h++){
-            for(int m=0; m<60; m++){
-                for(int s=0; s<60; s++) {
-                    System.out.printf("%02d:%02d:%02d\n", h, m, s);
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m++) {
+                for (int s = 0; s < 60; s++) {
+                    
+                    int hora12 = h % 12;
+                    if (hora12 == 0) {
+                        hora12 = 12;
+                    }
+
+                    String periodo = (h < 12) ? "AM" : "PM";
+
+                    System.out.printf("%02d:%02d:%02d %s\n", hora12, m, s, periodo);
 
                     try {
-                        Thread.sleep(100); // pausa de 1000 ms = 1 segundo real
+                        Thread.sleep(100); // 1000 para segundos reais
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
         }
+    }
+}
