e0ac96991ca7c7ee38f45e96ab1334a50e8e30e4
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-30 20:56:54 -0300

    Update relogio.java

--- relogio.java
+++ relogio.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-
 public class jonathan1 {
     public static void main(String[] args) {
 
         for (int h = 0; h < 24; h++) {
             for (int m = 0; m < 60; m++) {
                 for (int s = 0; s < 60; s++) {
-                    
+
                     int hora12 = h % 12;
                     if (hora12 == 0) {
                         hora12 = 12;
@@ -14,10 +12,12 @@ public class jonathan1 {
 
                     String periodo = (h < 12) ? "AM" : "PM";
 
-                    System.out.printf("%02d:%02d:%02d %s\n", hora12, m, s, periodo);
+                    System.out.print("\033[H\033[2J");
+
+                    System.out.printf("   %02d:%02d:%02d %s\n", hora12, m, s, periodo);
 
                     try {
-                        Thread.sleep(100); // 1000 para segundos reais
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
