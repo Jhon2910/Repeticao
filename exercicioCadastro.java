e39fa6aa1c5df95073bf5cf5139c7ff465e3c967
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-30 21:08:03 -0300

    Update exercicioCadastro.java

--- exercicioCadastro.java
+++ exercicioCadastro.java
@@ -1,56 +1,54 @@
 import java.util.Scanner;
 
-public class jonathan{
-    public static void main(String[]args){
-        /* F.A.Q realize o cadastro de um usuario com as seguintes informações :
-
-         Nome:Deve possur mais de 2 caracteres.
-
-         Idade:Deve estar entre 0 e 150.
-
-         Email:deve possuir o caracter arroba.
-
-         Senha:Deve conter número,letra minuscula,letra maiuscula e caracter especial("#","*","_","!","@").
-
-         Utilize as funções da classe character:
-         -isUpperCase
-         -isLowerCase
-         -isDigit
-         */
-
+public class jonathanExercico {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String nome;
+        String nome, email, senha;
         int idade;
-        char senha,email;
 
-
-        /*System.out.print("Digite seu nome: ");
-        String nome = sc.nextLine();
-        System.out.print("Digite sua idade: ");
-        int idade = sc.nextInt();
-        System.out.print("Digite o email (ex: usuario@gmail.com): ");
-        String email = sc.nextLine();
-        System.out.print("Digite a senha(Deve conter número,letra minuscula,letra maiuscula e caracter especial(#,*,_,!,@");*/
-
-        do {
-            if (sc.hasNextLine()){
+        while (true) {
+            System.out.print("Seu nome: ");
             nome = sc.nextLine();
-            System.out.println("Olá "+nome+" Bem vindo!");
+            if (nome.length() <= 2) {
+                System.out.println("Nome muito curto.\n");
+                continue;
             }
-            else{
-                System.out.println("Digite caracteres válidos!");
+
+            System.out.print("Sua idade: ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Digite uma idade válida.\n");
+                sc.next();
+                continue;
             }
             idade = sc.nextInt();
-            if (idade < 0 || idade > 150){
-                System.out.println("Idade inválida!");
+            sc.nextLine();
+            if (idade < 0 || idade > 150) {
+                System.out.println("Idade fora do limite.\n");
+                continue;
             }
-            else {
-                email = ;
 
+            System.out.print("Seu email: ");
+            email = sc.nextLine();
+            if (!email.contains("@")) {
+                System.out.println("Email precisa ter '@'.\n");
+                continue;
             }
+
+            System.out.print("Sua senha: ");
+            senha = sc.nextLine();
+
+            boolean temMaiuscula = senha.matches(".*[A-Z].*");
+            boolean temMinuscula = senha.matches(".*[a-z].*");
+            boolean temNumero    = senha.matches(".*[0-9].*");
+            boolean temEspecial  = senha.matches(".*[#*_!@].*");
+
+            if (!temMaiuscula || !temMinuscula || !temNumero || !temEspecial) {
+                System.out.println("A senha deve conter letra MAIÚSCULA, minúscula, número e símbolo (# * _ ! @).\n");
+                continue;
+            }
+            break;
         }
-        while ();
+        System.out.println("\nCadastro feito com sucesso. Bem-vindo, " + nome + "!");
+        sc.close();
     }
-
 }
